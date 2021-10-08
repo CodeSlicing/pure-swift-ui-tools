607f87253d17ad865146c430bb2cea7320ddd20c
Brett Best <6104381+Brett-Best@users.noreply.github.com>
2021-10-08 17:48:03 +1100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Brett-Best/pure-swift-ui.git", .branch("feature/MPP-Support")),
+        .package(url: "https://github.com/CodeSlicing/pure-swift-ui.git", .branch("feature/3.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
